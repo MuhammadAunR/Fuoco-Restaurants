0249d086ae3ca6989255b9222d7ad85791a98f17
M Aun Rasheed <greataun786@gmail.com>
2026-04-02 15:53:32 +0500

    fix; overflow due to  AOS

--- app/layout.js
+++ app/layout.js
@@ -41,7 +41,7 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${lora.variable} ${inter.variable} ${script.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col overflow-x-hidden">
         <AppWrapper>
           <ScrollToTopOnNavigate />
           <Toaster
